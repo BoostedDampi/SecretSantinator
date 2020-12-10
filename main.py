c689d38b34d4a84e35866d671e52c8fa3b0a9869
BoostedDampi <44726463+BoostedDampi@users.noreply.github.com>
2020-12-10 14:20:11 +0100

    Update main.py
    Removed Recursion and general overhaul

--- main.py
+++ main.py
@@ -1,56 +1,66 @@
 import random
 import os
 
-try:
-    names = []
 
-    dir_path = os.path.dirname(os.path.realpath(__file__)) + "/Secrets" #path to the folder where the names will be put in
+names = []
 
-    def santa_gen(names): #Outputs a shuffled list of names
+dir_path = os.path.dirname(os.path.realpath(__file__)) + "/Secrets" #path to the folder where the names will be put in
 
-        same_name = False
+def santa_gen(names): #Outputs a shuffled list of names
+
+    while True:
+        candidate = True
         r = random.sample(names, len(names)) #this shuffles the list
-
-        for n in range(len(names)): #this checks if some names picked themselves
+        for n in range(len(names)):
+            print(f"Checking '{names[n]}' against '{r[n]}'")
             if names[n] == r[n]:
-                same_name = True
+                candidate = False
+                print()
+                print("Reshuffling...")
+                break
+        if candidate == True:
+            return r
 
-        if same_name == True: #if some names picked themselves the function repeats
-            santa_gen(names)
-
-        return r #If all names are different the list will be returned
 
 
-    def send_letter(names, random_names):
 
-        try:
-            os.makedirs(dir_path, exist_ok=True) #this will create a folder in the same location as the script
 
-            for a in range(len(names)): #This bit creates a file for every name and writes his secret santa into it
-                file = f"{dir_path}/{names[a]}.txt"
-                f = open(file, "w")
-                f.write(random_names[a])
-                f.close()
+def send_letter(names, random_names):
 
-        except:
+    try:
+        os.makedirs(dir_path, exist_ok=True) #this will create a folder in the same location as the script
+
+        for a in range(len(names)): #This bit creates a file for every name and writes his secret santa into it
+            file = f"{dir_path}/{names[a]}.txt"
+            f = open(file, "w")
+            f.write(random_names[a])
             f.close()
 
+    except:
+        f.close()
+
+
+debug = False
+
+if debug == True:
+    names = input(">> ").split(", ")
+    santa = santa_gen(names)
+    print(f"This is the original list: \n {names} \nThis is the new list: \n {santa}")
+
+
+else:
     print("-"*50)
     print("Secret Santinator")
     print
     print("-"*50)
-    print("Scrivi i nomi dei partecipanti seguiti da una virgola e uno spazio.\n Es. David, Usai, Lollo")
+    print("Scrivi i nomi dei partecipanti seguiti da una virgola e uno spazio.\nEs. David, Usai, Lollo")
     names = input(">> ").split(", ")
-
+    print()
 
     send_letter(names, santa_gen(names))
+
     print()
     print("Completed! Check your new 'Secret' folder, and send the files to your friends.")
     print("If you cant delete the folder in the future, delete the files in it first.")
     print("Have fun, David")
-    input()
-
-except:
-
-    print("I don´t know what you have done, but please don´t do it again.")
     input()
